def26226c9905e40280c23dcab7cd85a47ead8e3
Kazin <kazin7853@qq.com>
2023-06-01 14:55:05 +0800

    feat: 实现UIToolkit btn点击事件

--- Assets/Scenes/ARInspection/RuntimeUI.cs
+++ Assets/Scenes/ARInspection/RuntimeUI.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class RuntimeUI : MonoBehaviour
 {
     private TextField idTextField;
+    private Button setValueBtn;
+    private UIDocument document;
     void Start()
     {
+        document = GetComponent<UIDocument>();
         SetId("abcd");
+        setValueBtn = document.rootVisualElement.Q("SetValueBtn") as Button;
+        setValueBtn.RegisterCallback<ClickEvent>(OnBtnClick);
     }
 
     public void SetId(string str)
     {
-        var document = GetComponent<UIDocument>();
         idTextField = document.rootVisualElement.Q("id") as TextField;
-
         idTextField.value = $"{str}";
+    }
+
+    void OnBtnClick (ClickEvent evt)
+    {
+        SetId("10086");
     }
 }
