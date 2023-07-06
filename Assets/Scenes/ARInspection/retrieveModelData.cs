a1eaf09096b60ecd4d467e9231f5c86d0216660a
Kazin <kazin7853@qq.com>
2023-07-06 10:25:04 +0800

    feat: 模型属性信息展示时可以滚动；加载管线模型

--- Assets/Scenes/ARInspection/retrieveModelData.cs
+++ Assets/Scenes/ARInspection/retrieveModelData.cs
@@ -6,7 +6,8 @@ public class RetrieveGltfData : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject gb = Importer.LoadFromFile("Assets/Scenes/ARInspection/pipe_brick_monkey.gltf");
+        // FIXME: 在手机上运行时找不到文件
+        GameObject gb = Importer.LoadFromFile("Assets/Scenes/ARInspection/model.gltf");
         gb.transform.position = new Vector3(0, 0, -6);
     }
 
